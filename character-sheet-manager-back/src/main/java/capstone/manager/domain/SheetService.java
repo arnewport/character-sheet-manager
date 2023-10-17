b38808475eae72e44c3b1d25765f818d356cde53
Andrew <anewport@outlook.com>
2023-10-17 14:12:41 -0400

    service, controller, error handling

--- character-sheet-manager-back/src/main/java/capstone/manager/domain/SheetService.java
+++ character-sheet-manager-back/src/main/java/capstone/manager/domain/SheetService.java
@@ -1,4 +1,71 @@
 package capstone.manager.domain;
 
+import capstone.manager.data.SheetRepository;
+import capstone.manager.models.Sheet;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SheetService {
+
+    private final SheetRepository repository;
+
+    public SheetService(SheetRepository repository) {
+        this.repository = repository;
+    }
+
+    public Sheet findById(int id) {
+        return repository.findById(id);
+    }
+
+    public Result<Sheet> create(Sheet sheet) {
+
+        Result<Sheet> result = validate(sheet);
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        if (sheet.getId() <= 0) {
+            result.addMessage("sheet id is required");
+            return result;
+        }
+
+        boolean success = repository.update(sheet);
+        if (!success) {
+            result.addMessage("could not update sheet id " + sheet.getId());
+        }
+
+        return result;
+    }
+
+    public Result<Sheet> update(Sheet sheet) {
+        Result<Sheet> result = validate(sheet);
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        if (sheet.getId() <= 0) {
+            result.addMessage("sheet id is required");
+            return result;
+        }
+
+        if (!repository.update(sheet)) {
+            result.addMessage("could not update sheet id " + sheet.getId());
+        }
+
+        return result;
+    }
+
+    public boolean deleteById(int id) {
+        return repository.deleteById(id);
+    }
+
+    // TODO: expand validation
+    public Result<Sheet> validate(Sheet sheet) {
+        Result<Sheet> result = new Result<>();
+        if (sheet == null) {
+            result.addMessage("sheet cannot be null");
+            return result;
+        }
+        return result;
+    }
 }
