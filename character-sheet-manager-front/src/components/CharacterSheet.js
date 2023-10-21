eaceb25cad2347f3f6a5670edacdd619b763bb6c
Andrew <anewport@outlook.com>
2023-10-21 13:29:24 -0400

    progress towards deletion of sheet based on user role

--- character-sheet-manager-front/src/components/CharacterSheet.js
+++ character-sheet-manager-front/src/components/CharacterSheet.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
-import { Link, useNavigate, useParams } from "react-router-dom";
-import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { Link, useParams } from "react-router-dom";
+import { Container, Row, Col, Form, Button, Modal } from 'react-bootstrap';
 // import useSheet from '../hooks/useSheet';
 
 const INITIAL_SHEET = {
@@ -19,6 +19,50 @@ function CharacterSheet() {
     const [sheet, setSheet] = useState(INITIAL_SHEET);
     const [errors, setErrors] = useState([]);
     const { id } = useParams();
+
+    // DELETION
+
+    const handleDeleteClick = (agentId) => {
+      setId(agentId);
+      setShowModal(true);
+  };
+  
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
+
+    // okay, since I'm making the deletion at the sheet
+    // I need to pass on the user id to here as well
+    // we'll need to pull the UserSheet and check if the user is OWNER
+    // if owner, we delete the UserSheet and the sheet; navigate back to home
+    // if editor, we delete the UserSheet but not the sheet; we just navigate back to home afterwards
+
+    const handleDelete = () => {
+      if (id !== null) {
+          const config = {
+              method: "DELETE",
+          };
+          fetch("http://localhost:8080/api/v1/sheet/" + id, config)
+              .then(response => {
+                  if (response.ok) {
+                      // success
+                      // delete all associated instances here
+                  } else {
+                      // failure
+                      return Promise.reject(
+                          new Error(`Unexpected status code ${response.status}`)
+                      );
+                  }
+              }).then(setId)
+              .catch(error => {
+                  console.error(error);
+              });  
+      }
+      handleCloseModal();
+      return;
+  }
+
+  //
 
     useEffect(() => {
       if (id) {
@@ -167,7 +211,30 @@ function CharacterSheet() {
             <Link to="/home" className="btn btn-warning">
               Home
             </Link>
+            <Button>
+              Share
+            </Button>
+            <Button>
+              Refresh
+            </Button>
+            <Button onClick={() => handleDeleteClick(id)}>
+              Delete
+            </Button>
           </Form>
+          <Modal show={showModal} onHide={handleCloseModal}>
+                <Modal.Header closeButton>
+                <Modal.Title>Confirm Deletion</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Are you sure you want to delete this sheet?</Modal.Body>
+                <Modal.Footer>
+                <Button className="btn btn-danger me-2" variant="primary" onClick={handleDelete}>
+                    Delete
+                </Button>
+                <Button className="btn btn-warning" variant="secondary" onClick={handleCloseModal}>
+                    Cancel
+                </Button>
+                </Modal.Footer>
+            </Modal>
         </Container>
       );
     };
